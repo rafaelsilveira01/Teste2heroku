2269a4a3c650099b06a9f26cfd68062b847a6a77
Rafael Silveira <rafaell.ssouza@hotmail.com>
2022-08-16 15:12:35 -0300

    teste1

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,50 @@
+package com.generation.blogpessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Descrição é obrigatório e não pode conter espaços em branco") // Preenchimento obrigatorio
+	@Size(min = 5 , max = 1000) // Validar os caracteres
+	private String descricao;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
